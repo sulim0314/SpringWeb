cac19387e0aaf57d9e987620fbee1125eb8a4849
Sulim <sulim0314@naver.com>
2023-05-09 11:34:30 +0800

    SpringWeb 공부 4-3

---
+++ java/com/shop/mapper/ProductMapper.java
@@ -0,0 +1,12 @@
+package com.shop.mapper;
+
+import java.util.List;
+
+import com.shop.model.ProductVO;
+
+public interface ProductMapper {
+	
+	int productInsert(ProductVO item);
+	List<ProductVO> getProducts();
+
+}
