e45a78470fcc4a4f7fb8fb99b0c8135070695570
Sulim <sulim0314@naver.com>
2023-05-10 23:49:49 +0900

    SpringWeb 공부 5,6

---
+++ java/com/my/myapp/ProductController.java
@@ -0,0 +1,48 @@
+package com.my.myapp;
+
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.shop.model.ProductVO;
+import com.shop.service.ShopService;
+
+import lombok.extern.log4j.Log4j;
+
+@Controller
+@Log4j
+public class ProductController {
+	
+	@Inject
+	private ShopService shopService;
+	 
+	//pspec별로 상품목록 가져오기
+	@GetMapping("/prodPspec")
+	public String productByPspec(Model m, @RequestParam(name="pspec", defaultValue="HIT") String pspec) {
+		log.info("pspec: "+pspec);
+		List<ProductVO> pList=shopService.selectByPspec(pspec);
+		m.addAttribute("pList",pList);
+		m.addAttribute("pspec",pspec);
+		return "shop_module/mallHit";
+		//WEB-INF/views/shop/mallHit.jsp
+	}
+	
+	@GetMapping("/prodDetail")
+	public String productView(Model m,@RequestParam(name="pnum", defaultValue="0") int pnum) {
+		log.info("pnum="+pnum);
+		if(pnum==0) {
+			return "redirect:index";
+		}
+		ProductVO item=shopService.selectByPnum(pnum);
+		m.addAttribute("item",item);
+		
+		return "shop/prodView";
+		//WEB-INF/views/shop/prodView.jsp
+	}
+
+}
--- webapp/WEB-INF/views/index.jsp
+++ webapp/WEB-INF/views/index.jsp
@@ -1,5 +1,13 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+<%@ taglib prefix="c"  uri="http://java.sun.com/jsp/jstl/core"%>
 
+<!--  HIT상품 보여주기 -->
+<c:import url="/prodPspec">
+	<c:param name="pspec" value="HIT"/>
+</c:import>sdf
 
-<h1>Index Page</h1>
+<!--  NEW상품 보여주기 -->
+<c:import url="/prodPspec">
+	<c:param name="pspec" value="NEW"/>
+</c:import> 
