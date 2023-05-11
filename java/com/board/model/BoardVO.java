21421a654bd0033c068d38af639c005c84a12603
Sulim <sulim0314@naver.com>
2023-05-11 21:30:58 +0900

    SpringWeb 공부 7

---
+++ java/com/board/model/BoardVO.java
@@ -0,0 +1,32 @@
+package com.board.model;
+
+import java.sql.Date;
+
+import lombok.Data;
+
+@Data
+public class BoardVO {
+	// 글쓰기, 글수정하기, 답변글쓰기
+	private String mode;
+	
+	// 글쓰기: write, 답변글쓰기: rewrite, 글수정:edit
+	private int num;
+	private String userid;
+	private String passwd;
+	private String subject;
+	private String content;
+	
+	private Date wdate;
+	private int readnum;
+	
+	private String filename; //물리적 파일명 uuid_파일명
+	private String originFilename; //원본 파일명
+	private long filesize;
+	
+	//답변형 게시판에서 필요한 프로퍼티
+	private int refer; //글그룹 번호
+	private int lev; //답변 레벨
+	private int sunbun; //같은 글 그룹 내의 순서
+	
+	
+}
--- webapp/WEB-INF/views/index.jsp
+++ webapp/WEB-INF/views/index.jsp
@@ -3,11 +3,7 @@
 <%@ taglib prefix="c"  uri="http://java.sun.com/jsp/jstl/core"%>
 
 <!--  HIT상품 보여주기 -->
-<c:import url="/prodPspec">
-	<c:param name="pspec" value="HIT"/>
-</c:import>sdf
+<%@ include file="/inc/mallHit.jspf" %>
 
 <!--  NEW상품 보여주기 -->
-<c:import url="/prodPspec">
-	<c:param name="pspec" value="NEW"/>
-</c:import> 
+<%@ include file="/inc/mallNew.jspf" %>
