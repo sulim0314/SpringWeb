1a14fb0261b936ec819f4e86c9e64ea646e0deaf
Sulim <sulim0314@naver.com>
2023-05-15 22:22:47 +0900

    SpringWeb 공부 9 최종

--- java/com/board/service/BoardServiceImpl.java
+++ java/com/board/service/BoardServiceImpl.java
@@ -28,8 +28,8 @@ public class BoardServiceImpl implements BoardService {
 
 	@Override
 	public List<BoardVO> selectBoardAllPaging(PagingVO paging) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		return this.boardMapper.selectBoardAllPaging(paging);
 	}
 
 	@Override
@@ -40,8 +40,7 @@ public class BoardServiceImpl implements BoardService {
 
 	@Override
 	public int getTotalCount(PagingVO paging) {
-		// TODO Auto-generated method stub
-		return 0;
+		return this.boardMapper.getTotalCountPaging(paging);
 	}
 
 	@Override
